3b3d7ca8f50d1f4ebc4991ff8bfac81ffa2ffdd0
crixusraze <30280754+crixusraze@users.noreply.github.com>
2022-08-04 18:19:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit 
-# import pandas
+import pandas
 import snowflake.connector
 from urllib.error import urlerror
 
@@ -24,7 +24,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-# import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi")
 
 # write your own comment -what does the next line do? 
