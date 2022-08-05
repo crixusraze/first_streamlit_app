3eee8beb358f0f1d40edbc05cff1dba26128361f
crixusraze <30280754+crixusraze@users.noreply.github.com>
2022-08-05 18:51:19 +0530

    fruitname submission

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,19 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
   
   
+   
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit')")
+    return "thanks for adding" + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+if streamlit.button('Add a fruit to list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
     
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
 
-my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit')")
