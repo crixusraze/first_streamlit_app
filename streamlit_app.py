2846015644e40314b4d8763434cda260fd05a89f
crixusraze <30280754+crixusraze@users.noreply.github.com>
2022-08-05 15:59:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,4 @@ streamlit.header("Fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
